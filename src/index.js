faa87b747d0adb0ec383c85e62aed459f2faf3c9
AnthonyTC89 <ptonyptc@gmail.com>
2019-11-25 17:02:55 -0500

    showProject after events

--- src/index.js
+++ src/index.js
@@ -9,46 +9,46 @@ const notes = document.querySelector('#notes');
 const submit = document.querySelector('#submit');
 const clear = document.querySelector('#clear');
 const display = document.querySelector('#display');
-
 const projects = localStorage.getItem('projects') ? JSON.parse(localStorage.getItem('projects')) : [];
+
+function showProjects() {
+  projects.forEach((item) => {
+    const titleDisplay = document.createElement('h2');
+    const descDisplay = document.createElement('p');
+    const dateDisplay = document.createElement('p');
+    const prioDisplay = document.createElement('p');
+    const noteDisplay = document.createElement('p');
+
+    titleDisplay.innerHTML = item.title;
+    descDisplay.innerHTML = item.description;
+    dateDisplay.innerHTML = item.dueDate;
+    prioDisplay.innerHTML = item.priority;
+    noteDisplay.innerHTML = item.notes;
+
+    display.appendChild(titleDisplay);
+    display.appendChild(descDisplay);
+    display.appendChild(dateDisplay);
+    display.appendChild(prioDisplay);
+    display.appendChild(noteDisplay);
+  });
+}
 
 function handleClick() {
   const project = Project(title.value, description.value, date.value,
     priority.checked ? priority = true : priority = false, notes.value);
 
   projects.push(project);
-  
   localStorage.setItem('projects', JSON.stringify(projects));
-
-
-
+  showProjects();
 }
 
 function clearLocalStore() {
   localStorage.clear();
+  showProjects();
   console.log(localStorage);
 }
 
 submit.addEventListener('click', handleClick);
 clear.addEventListener('click', clearLocalStore);
 
-projects.forEach((item) => {
-  console.log(item);
-  const titleDisplay = document.createElement('h2');
-  const descDisplay = document.createElement('p');
-  const dateDisplay = document.createElement('p');
-  const prioDisplay = document.createElement('p');
-  const noteDisplay = document.createElement('p');
-
-  titleDisplay.innerHTML = item.title;
-  descDisplay.innerHTML = item.description;
-  dateDisplay.innerHTML = item.dueDate;
-  prioDisplay.innerHTML = item.priority;
-  noteDisplay.innerHTML = item.notes;
-
-  display.appendChild(titleDisplay);
-  display.appendChild(descDisplay);
-  display.appendChild(dateDisplay);
-  display.appendChild(prioDisplay);
-  display.appendChild(noteDisplay);
-});
+showProjects();
