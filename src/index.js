a0c32ae91ac5bfb22d197a4ba7ac0c70a8ab1303
AnthonyTC89 <ptonyptc@gmail.com>
2019-11-26 17:28:44 -0500

    task saved

--- src/index.js
+++ src/index.js
@@ -4,19 +4,16 @@ import Task from './tasks';
 import './style/style.css';
 
 // Task inputs
-const tasksForm = document.querySelector('#new-task');
-const taskTitle = document.querySelector('#task-title');
-const description = document.querySelector('#description');
-const date = document.querySelector('#date');
-let priority = document.querySelector('#priority');
-const notes = document.querySelector('#notes');
-const submitTask = document.querySelector('#submit');
+//const tasksForm = document.querySelector('#new-task');
+
+// const submitTask = document.querySelector('#submit');
 
 // General elements
 const clear = document.querySelector('#clear');
 const displayProject = document.querySelector('#display-project');
-const containerTasks = document.querySelector('#display-tasks');
-
+const displayTasks = document.querySelector('#display-tasks');
+const newTask = document.querySelector('#new-task');
+let lsProjects = [];
 
 // Project inputs
 const projectTitle = document.querySelector('#project-title');
@@ -24,34 +21,67 @@ const submitProject = document.querySelector('#submit-project');
 const cancelProject = document.querySelector('#cancel-project');
 // const newProjectForm = document.querySelector('.new-project');
 
-// newProjectForm.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   addProject();
-// });
+function addTask(elem) {
+  let key = elem.dataset.name;
 
-function addTask(key) {
-  const theKey = key.dataset.name
-  tasksForm.classList.toggle('hidden');
-  
+  const projectSelected = lsProjects.find((item) => item.title == key);
 
-  localStorage.setItem(theKey, JSON.stringify(Task('Task 2', 'Description 2')));
-  const projectWithTasks = JSON.parse(localStorage.getItem(theKey));
-  console.log(projectWithTasks);
+  const taskTitle = document.querySelector('#task-title');
+  const description = document.querySelector('#description');
+  const date = document.querySelector('#date');
+  const priority = document.querySelector('#priority');
+  const notes = document.querySelector('#notes');
+
+  const taskValues = Task(taskTitle.value, description.value, date.value, priority.value, notes.value);
+
+  projectSelected.tasks.push(taskValues);
+  taskTitle.value = '';
+  description.value = '';
+  date.value = '';
+  notes.value = '';
+  showTasks(elem);
+  newTask.classList.add('hidden');
 }
+
+function showTaskForm(elem) {
+  newTask.innerHTML = `
+      <input type="text" placeholder="Title" id="task-title" required><br>
+      <input type="text" placeholder="Description" id="description"><br>
+      <input type="date" id="date"><br>
+      <label id="checkbox-label">High Priority:</label>
+
+      <select id="priority">
+        <option value="Low">Low</option> 
+        <option value="Normal" selected>Normal</option>
+        <option value="High">High</option>
+      </select><br>
+
+      <textarea name="notes" id="notes" cols="30" rows="10"></textarea><br>
+      </br>`;
+
+  newTask.classList.toggle('hidden');
+  const btnSummit = document.createElement('button');
+  btnSummit.addEventListener('click', () => addTask(elem));
+  btnSummit.innerHTML = 'Add Task'
+  newTask.appendChild(btnSummit);
+  
+}
+
+
 
 // Function to show the project's related tasks
 function showTasks(elem){
-  const key = elem.dataset.name;
-  const projectTasks = JSON.parse(localStorage.getItem(key));
-
-  containerTasks.innerHTML = '';
+  let key = elem.dataset.name;
+  displayTasks.innerHTML = '';
 
   const btnNewTask = document.createElement('button');
   btnNewTask.innerHTML = 'New Task';
-  btnNewTask.addEventListener('click', addTask(elem));
-  containerTasks.appendChild(btnNewTask);
+  btnNewTask.addEventListener('click', () => showTaskForm(elem));
+  displayTasks.appendChild(btnNewTask);
   
-  projectTasks.forEach((task) => {
+  const projectSelected = lsProjects.find((item) => item.title == key);
+
+  projectSelected.tasks.forEach((task) => {
     const taskDetails = document.createElement('div');
     taskDetails.setAttribute('class', 'tasks-list');
     taskDetails.innerHTML += ` <h3> ${task.title} </h3>
@@ -59,47 +89,50 @@ function showTasks(elem){
       <p>DueDate: ${task.dueDate} </p>
       <p>Priority: ${task.priority} </p>
       <p>Notes: ${task.notes} </p>`;
-    containerTasks.appendChild(taskDetails);
-    
+    displayTasks.appendChild(taskDetails);
   });
 
 }
 
 // Funtion to show the created projects
 function showProjects() {
-  const ptStore = Object.keys(localStorage);
+  if (localStorage.length == 0) return;
+  lsProjects = JSON.parse(localStorage.getItem('projects'));
+
   displayProject.innerHTML = ''; // clear div
-  for (const title of ptStore) {
+  for (const project of lsProjects) {
     const h2 = document.createElement('h2');
     h2.setAttribute('class', 'project-title');
-    h2.setAttribute('data-name', title );
-    h2.innerHTML = title;
+    h2.setAttribute('data-name', project.title );
+    h2.innerHTML = project.title;
     displayProject.appendChild(h2);
   }
   const projectH2 = document.querySelectorAll('.project-title');
   projectH2.forEach((elem) => {
     elem.addEventListener('click', () => showTasks(elem));
   });
+
 }
 
 // Function to add a new project
 function addProject() {
   if (!projectTitle.value) return alert('Title must be provided!');
-  const project = Project(projectTitle.value);
+  let project = Project(projectTitle.value);
 
-  // project.addTask(Task('Task 1', 'Description'));
-  // project.addTask(Task('Task 2', 'Description 2'));
-
-  localStorage.setItem(project.title, JSON.stringify(project.tasks));
+  lsProjects.push(project);
+  
+  localStorage.setItem("projects", JSON.stringify(lsProjects));
   showProjects();
-
 }
 
 // function to clear localStorage
 function clearLocalStore() {
-  if (!localStorage) return;
+  if (localStorage.length == 0) return;
   localStorage.clear();
-  showProjects()
+  lsProjects = [];
+  displayProject.innerHTML = '';
+  console.log(localStorage);
+  showProjects();
 }
 
 
